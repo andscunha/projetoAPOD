0e5c24026315a293b38891a7d4bd181fab88aaa6
Anderson <ands1776@gmail.com>
2021-12-09 06:02:53 -0300

    Header versão mobile feito

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// Parte do Menu Mobile
+
 const btnMobile = document.getElementById('btn-mobile')
 
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
+
     const nav = document.getElementById('nav')
     nav.classList.toggle('active')
+
     const active = nav.classList.contains('active')
     event.currentTarget.setAttribute('aria-expanded', active);
 
