987294ee025606bc3d90b7e26f1ea8ddc2f56262
Anderson <ands1776@gmail.com>
2021-12-11 18:54:54 -0300

    Finalizando

--- script.js
+++ script.js
@@ -21,3 +21,26 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu)
 btnMobile.addEventListener('touchstart', toggleMenu)
+
+
+//Execução do API
+
+let botao = $('#botao')
+
+botao.on('click',()=>{
+    $.ajax({url:`https://api.nasa.gov/planetary/apod?api_key=zWE14xjDLkHUsGzoGuurWgKewmgY2M2Scqk2ofTI&date=${date.value}`,
+        success: function(result){
+
+            $('#title').html(`${result.title}`)
+
+            $('#imagem').attr('src',(result.hdurl))
+
+            $('#texto').html(`${result.explanation}`)
+
+            $('#autor').html(` ~ ${result.copyright}`)
+
+            console.log(result)
+
+     }
+    });
+  });
